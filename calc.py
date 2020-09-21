f2bf7572895cd17aa00c1274aea93dc6f2d7b9a8
Mattias Lantz Cronqvist <mattias@cronqvist.eu>
2020-09-21 18:19:45 +0200

    computer switch again...

--- calc.py
+++ calc.py
@@ -1,35 +1,46 @@
 import sys
 
-registers = {} 
-symbols = {} 
 
 
-
-def add_operation(op, func):
-    symbols[op] = func
 
 # How to add more operators... just provide a callable and map it to a name
-add_operation('add', lambda lhs, rhs: lhs + rhs)
-add_operation('subtract', lambda lhs, rhs: lhs - rhs)
-add_operation('multiply', lambda lhs, rhs: lhs * rhs)
-add_operation('division', lambda lhs, rhs: lhs / rhs)
+# add_operation('add', lambda lhs, rhs: lhs + rhs)
+# add_operation('subtract', lambda lhs, rhs: lhs - rhs)
+# add_operation('multiply', lambda lhs, rhs: lhs * rhs)
+# add_operation('division', lambda lhs, rhs: lhs / rhs)
 
 class Register():
 
     def __init__(self):
         self.children = list()
 
+    # checks wheter the child is already inside the parent
+    def check_children(self, parent):
+        children = parent.get_all_children()
+        # print(type(children))
+        
+        # for child in children:
+        #     print(child.name)
+
+        if self in children:
+            # print("error, cyclic dependency")
+            return False
+
+        return True
+
+
     def valid_add(self, operation):
         if isinstance(operation.val, Register):
-            children = operation.val.get_all_children()
-            if self in children:
-                print("error, cyclic dependency")
+
+            # checks whether both child and parent register contains eachother
+            if not self.check_children(operation.val) or not operation.val.check_children(self):
                 return False
-            
+
         return True
 
     def add(self, operation):
         if self.valid_add(operation):
+            # print("adding")
             self.children.append(operation)
         
         return True
@@ -41,16 +52,17 @@ class Register():
             if isinstance(operation.val, Register):
                 children.append(operation.val)
 
-                children.append(operation.val.get_all_children())
+                for child in operation.val.get_all_children():
+                    children.append(child) # flatten...
 
         return children
 
 
-    def eval(self):
+    def eval(self, symbols):
         sum = 0
         for operation in self.children:
             # looks up the operation and calls the appropriate function
-            sum = symbols[operation.op](sum, operation.eval())
+            sum = symbols[operation.op](sum, operation.eval(symbols))
 
         return sum
  
@@ -64,114 +76,135 @@ class Operation():
         self.op = op
         self.val = val
 
-    def eval(self):
+    def eval(self, symbols):
         # if it's a Register object do a recursive call down the tree
         if isinstance(self.val, Register):
-            return self.val.eval()
+            return self.val.eval(symbols)
         else:
             return self.val
 
 class Calculator():
 
 
+    def add_operation(self, op, func):
+        self.symbols[op] = func
+
     def __init__(self):
         self.registers = {} 
         self.symbols = {} 
 
+        self.add_operation('add', lambda lhs, rhs: lhs + rhs)
+        self.add_operation('subtract', lambda lhs, rhs: lhs - rhs)
+        self.add_operation('multiply', lambda lhs, rhs: lhs * rhs)
+        self.add_operation('division', lambda lhs, rhs: lhs / rhs)
+
+
+    @staticmethod
+    def valid_register_name(name):
+        # Any name consisting of alphanumeric characters should be allowed as register names.
+        return name.isalnum()
 
 
     def valid_operation(self, name):
-        return name in symbols
+        return name in self.symbols
 
-def valid_register(name):
-    # Any name consisting of alphanumeric characters should be allowed as register names.
-    return name.isalnum()
+    def register_exists(self, name):
+        return name in self.registers
+        
+    def add_register(self, name):
+        if self.valid_register_name(name):
+            # print("adding register " + name)
+            self.registers[name] = Register()
 
-def register_exists(name):
-    return name in registers
-    
-def add_register(name):
-    if valid_register(name):
-        # print("adding register " + name)
-        registers[name] = Register()
+    def valid_command(self, line):
+        command = line.lower().rstrip().split(" ")
 
-def valid_command(line):
-    command = line.lower().rstrip().split(" ")
+        if len(command) == 1: # should be a quit command
+            if (command[0] == "quit"):
+                return True
 
-    if len(command) == 1: # should be a quit command
-        if (command[0] == "quit"):
-            return True
-
-    if len(command) == 2: # should be a print command
-        reg = command[1]
-        if register_exists(reg) and command[0] == "print":
-            return True
-
-    if len(command) == 3: # operation command, reg + op + reg/val
-        dest = command[0]   
-        src = command[2]
-        op = command[1]
-
-        if valid_register(dest) and valid_register(src) and valid_operation(op):
-            if register_exists(dest) and register_exists(src):
-                # need to check for cycles if both registrers already exists.
-                dest_reg = registers[dest]
-                if not dest_reg.valid_add(Operation(op, registers[src])):
-                    return False
-            return True
-
-    return False
-
-def run_command(line):
-    # All input should be case insensitive, also removes trailing newlines from input...
-    command = line.lower().rstrip().split(" ")
-
-    if valid_command(line):
-        if len(command) == 1: # quit
-            exit()
-            # return "quit"
-
-        if len(command) == 2: # print 
+        if len(command) == 2: # should be a print command
             reg = command[1]
-            val = registers[reg].eval()
-            print(val)
-            # return val
+            if self.register_exists(reg) and command[0] == "print":
+                return True
 
-        if len(command) == 3:
-            dest = command[0]
-            op = command[1]
+        if len(command) == 3: # operation command, reg + op + reg/val
+            dest = command[0]   
             src = command[2]
+            op = command[1]
 
-            if not register_exists(dest):
-                add_register(dest)
-            dest_reg = registers[dest]
+            if self.valid_register_name(dest) and self.valid_register_name(src) and self.valid_operation(op):
+                if self.register_exists(dest) and self.register_exists(src):
+                    # need to check for cycles if both registrers already exists.
+                    dest_reg = self.registers[dest]
+                    if not dest_reg.valid_add(Operation(op, self.registers[src])):
+                        return False
+                return True
 
-            if register_exists(src):
-                dest_reg.add(Operation(op, registers[src]))
-            else:
-                if src.isnumeric(): # if no register previously exists and only numbers assume value-operation
-                    dest_reg.add(Operation(op, int(src)))
-                else: # else create new register...
-                    add_register(src)
-                    dest_reg.add(Operation(op, registers[src]))
+        return False
 
-    else:
-        # Invalid commands can be ignored, but should be logged to the console.
-        print(line.rstrip())
-        # return line.rstrip()
+    def run_command(self, line):
+        # All input should be case insensitive, also removes trailing newlines from input...
+        command = line.lower().rstrip().split(" ")
+
+        if self.valid_command(line):
+            if len(command) == 1: # quit
+                #exit()
+                return "quit"
+
+            if len(command) == 2: # print 
+                reg = command[1]
+                val = self.registers[reg].eval(self.symbols)
+                # print(val)
+                return val
+
+            if len(command) == 3:
+                dest = command[0]
+                op = command[1]
+                src = command[2]
+
+                if not self.register_exists(dest):
+                    self.add_register(dest)
+                dest_reg = self.registers[dest]
+
+                if self.register_exists(src):
+                    dest_reg.add(Operation(op, self.registers[src]))
+                else:
+                    if src.isnumeric(): # if no register previously exists and only numbers assume value-operation
+                        dest_reg.add(Operation(op, int(src)))
+                    else: # else create new register...
+                        self.add_register(src)
+                        dest_reg.add(Operation(op, self.registers[src]))
+
+        else:
+            # Invalid commands can be ignored, but should be logged to the console.
+            # print(line.rstrip())
+            return line.rstrip()
 
 
-def handle_input(istream):
-    # output = list()
-    for line in istream:
-        # val = run_command(line)
-        run_command(line)
-        # if val is not "quit":
-        #     output.append(run_command(line))
+    def handle_input(self, istream):
+        output = list()
 
-    # When accepting input from file, it should not be necessary to include
-    # quit to exit the program. Adding a quit command in the end to make sure the program exits.
-    run_command("quit")
+        # reset old registers
+        self.registers = {}
+
+        # The program should either take its input from the standard input stream, or from a file. When
+        # the program is launched with one command line argument, input should be read from the file
+        # specified in the argument. When accepting input from file, it should not be necessary to include
+        # quit to exit the program.
+
+        # I interpret this as if quit is not supplied by stdin it should never exit? it sounds a bit strange so i just take whats given
+        # and process it.
+
+        for line in istream:
+            # val = run_command(line)
+            val = self.run_command(line)
+            if val:
+                if val == "quit":
+                    return output
+
+                output.append(val)
+        return output
 
     # return output 
 
@@ -179,14 +212,18 @@ def handle_input(istream):
 # The program should either take its input from the standard input stream, or from a file. When
 # the program is launched with one command line argument, input should be read from the file
 # specified in the argument.
-if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        f = open(sys.argv[1], 'r')
-        # maybe check if file exists...
-    else:
-        f = sys.stdin
+# if __name__ == '__main__':
+#     if len(sys.argv) > 1:
+#         f = open(sys.argv[1], 'r')
+#         # maybe check if file exists...
+#     else:
+#         f = sys.stdin
 
-    handle_input(f)
+#     calc = Calculator()
+#     output = calc.handle_input(f)
+
+#     for val in output:
+#         print(val)
 #     print(output)
 # for command in commands:
 #     print(command)
@@ -219,3 +256,24 @@ if __name__ == '__main__':
 # print(len(result.get_all_children()))
 
 # costs.add(Operation("add", result))
+
+# cyclic
+r1 = Register()
+r2 = Register()
+r3 = Register()
+
+r1.add(Operation("add", r2))
+r2.add(Operation("add", r3))
+
+# should fail
+r3.add(Operation("add", r1))
+r2.add(Operation("add", r1))
+
+# r1 = Register("r1")
+# r2 = Register("r2")
+# r3 = Register("r3")
+
+# r1.add(Operation("add", r2))
+# r2.add(Operation("add", r3))
+# r3.add(Operation("add", r1))
+
--- calc_tests.py
+++ calc_tests.py
@@ -1,37 +1,57 @@
 import unittest
 import sys
 import calc
-
+from io import StringIO
 
 class TestCalc(unittest.TestCase):
 
+    def setUp(self):
+        self.calc = calc.Calculator()
+
     def test_valid_command(self):
-        self.assertTrue(calc.valid_command("quit"))
-        self.assertTrue(calc.valid_command("Quit")) # case insensitive
-        self.assertFalse(calc.valid_command("??"))
+        self.assertTrue(self.calc.valid_command("quit"))
+        self.assertTrue(self.calc.valid_command("Quit")) # case insensitive
+        self.assertFalse(self.calc.valid_command("??"))
 
         # should not be able to print before added, this is how I assumed it should work
-        self.assertFalse(calc.valid_command("print a"))
+        self.assertFalse(self.calc.valid_command("print a"))
         # should be able to print added (mentioned before) register
-        calc.add_register("a")
-        self.assertTrue(calc.valid_command("print a")) 
-        self.assertFalse(calc.valid_command("pr1nt a"))
+        self.calc.add_register("a")
+        self.assertTrue(self.calc.valid_command("print a")) 
+        self.assertFalse(self.calc.valid_command("pr1nt a"))
 
         # operation tests
-        calc.add_register("b")
-        self.assertTrue(calc.valid_command("a add b"))
-        self.assertTrue(calc.valid_command("a add c"))
-        self.assertTrue(calc.valid_command("d add c"))
-        self.assertTrue(calc.valid_command("d add 1"))
-        self.assertFalse(calc.valid_command("d asd 1"))
-        self.assertTrue(calc.valid_command("d multiply 1"))
-        self.assertTrue(calc.valid_command("d subtract 1"))
+        self.calc.add_register("b")
+        self.assertTrue(self.calc.valid_command("a add b"))
+        self.assertTrue(self.calc.valid_command("a add c"))
+        self.assertTrue(self.calc.valid_command("d add c"))
+        self.assertTrue(self.calc.valid_command("d add 1"))
+        self.assertFalse(self.calc.valid_command("d asd 1"))
+        self.assertTrue(self.calc.valid_command("d multiply 1"))
+        self.assertTrue(self.calc.valid_command("d subtract 1"))
 
     def test_case_1(self):
-        t1 = open('test1.txt', 'r')
-        output = calc.handle_input(t1)
-        t1.close()
-        self.assertEquals(output, "80")
+        # from the pdf
+        s1 = StringIO("A add 2\nA add 3\nprint A\nB add 5\nB subtract 2\nprint B\nA add 1\nprint A\nquit")
+        actual = self.calc.handle_input(s1)
+        expected = [5, 3, 6]
+        self.assertListEqual(actual, expected)
 
+        s2 = StringIO("result add revenue\nresult subtract costs\nrevenue add 200\ncosts add salaries\nsalaries add 20\nsalaries multiply 5\ncosts add 10\nprint result\nQUIT\n")
+        actual = self.calc.handle_input(s2)
+        expected = [90]
+        self.assertListEqual(actual, expected)
+
+    def test_cycle(self):
+        input = StringIO("r1 add r2\nr2 add r3\nr3 add r1\nr3 add r1")
+
+        # r1 -> r2 -> r3
+        # should not be possible to add r3 to r1
+        # should not be possible to add r1 to r3
+        # this is because they are already dependent of eachother in some way
+
+        actual = self.calc.handle_input(input)
+        expected = ["r3 add r1", "r3 add r1"] # two last adds should fail 
+        self.assertListEqual(actual, expected)
 
 unittest.main()
